2a1b903b506bb23c18a942375f87d129925d6919
Eduardo Orrego <eduardo.orrego@outlook.com>
2026-01-19 16:10:46 -0500

    A comment is added to describe the example.A comment is added to describe the example

--- src/main/java/org/xuaxpedia/datetime/DatetimeDemo01.java
+++ src/main/java/org/xuaxpedia/datetime/DatetimeDemo01.java
@@ -7,6 +7,14 @@ import java.time.format.DateTimeFormatter;
 
 public class DatetimeDemo01 {
   public static void main(String[] args) {
+    /*
+      * Java 8 Date and Time API
+      * LocalDate - represents a date (year, month, day)
+      * LocalTime - represents a time (hour, minute, second, nanosecond)
+      * LocalDateTime - represents both date and time
+      * DateTimeFormatter - used for formatting and parsing date-time objects
+      * Examples of creating, formatting, and extracting data from date-time objects
+    */
 
     System.out.println("LocalDate - the current date : " + LocalDate.now());
     System.out.println("LocalTime - the current time : " + LocalTime.now());
